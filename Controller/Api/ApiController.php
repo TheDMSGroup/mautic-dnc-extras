0f1b61fc95852376bc5b75ddff19091dc9497178
scottshipman <k.scott.shipman@gmail.com>
2019-04-12 11:59:00 -0400

    [ENG-878] fix type hint

--- Controller/Api/ApiController.php
+++ Controller/Api/ApiController.php
@@ -14,6 +14,7 @@ namespace MauticPlugin\MauticDoNotContactExtrasBundle\Controller\Api;
 use Mautic\ApiBundle\Controller\CommonApiController;
 use MauticPlugin\MauticDoNotContactExtrasBundle\Entity\DncListItem;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class ApiController.
